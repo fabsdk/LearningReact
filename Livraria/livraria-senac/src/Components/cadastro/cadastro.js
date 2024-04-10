627b5f58e813b0054ffc0bf99927a5a964688634
Fabiana Sayuri <fabianakamamoto@hotmail.com>
2024-04-10 20:17:09 -0300

    adicionando novas informacoes

--- Livraria/livraria-senac/src/Components/cadastro/cadastro.js
+++ Livraria/livraria-senac/src/Components/cadastro/cadastro.js
@@ -1,34 +1,29 @@
 import React from "react";
-import { Titulo, Input, Label, InputUsuario } from "../styles/Texts";
+import { Titulo, Input, Label } from "../styles/Texts";
 import { Button, ButtonCadastro} from "../styles/Button";
 import { Container, FormRow } from "../styles/Containers";
 
 const Cadastro = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const value = Object.fromEntries(data.entries());
-    console.log(value);
-  };
-
   return (
     <div>
-      <Titulo>Cadastro</Titulo>
-      <form onSubmit={handleSubmit}>
-        <FormRow>
-          <Label>Nome:</Label>
-          <InputUsuario type="text" name="nome" />
-        </FormRow>
-        <FormRow>
-          <Label>Email:</Label>
-          <Input type="email" name="email" />
-        </FormRow>
-        <FormRow>
-          <Label>Senha:</Label>
-          <Input type="password" name="senha" />
-        </FormRow>
-        <ButtonCadastro type="submit">Cadastrar</ButtonCadastro>
-      </form>
+      <Titulo>Bem-Vindo, faça seu cadastro</Titulo>
+      <Container>
+        <form>
+          <FormRow>
+            <Label>Nome:</Label>
+            <Input type="text" name="nome" />
+          </FormRow>
+          <FormRow>
+            <Label>Email:</Label>
+            <Input type="email" name="email" />
+          </FormRow>
+          <FormRow>
+            <Label>Senha:</Label>
+            <Input type="password" name="senha" />
+          </FormRow>
+          <ButtonCadastro type="submit">Cadastrar</ButtonCadastro>
+        </form>
+      </Container>
     </div>
   );
 };
